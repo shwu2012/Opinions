a81ad6cb9cdce4a541c5dab6e4e698c0f51105a8
Shuang Wu <shuangwu2011@gmail.com>
2012-05-07 22:23:56 -0700

    remove useless files

--- src/edu/sjsu/students/shuangwu/opinions/dao/AccountDao.java
+++
@@ -1,8 +0,0 @@
-package edu.sjsu.students.shuangwu.opinions.dao;
-
-import edu.sjsu.students.shuangwu.opinions.domain.Account;
-
-public interface AccountDao {
-	Account create(Account account);
-	Account getById(long userId);
-}
--- src/edu/sjsu/students/shuangwu/opinions/dao/VoteActionDao.java
+++
@@ -1,7 +0,0 @@
-package edu.sjsu.students.shuangwu.opinions.dao;
-
-import edu.sjsu.students.shuangwu.opinions.domain.VoteAction;
-
-public interface VoteActionDao {
-	VoteAction create(VoteAction voteAction);
-}
